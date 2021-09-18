c542e1e63b8a5921a6dfb5c859e14d24925e895d
Vito De Tullio <vito.detullio@gmail.com>
2021-09-18 16:05:19 +0200

    preload right path

--- mypyui/__init__.py
+++ mypyui/__init__.py
@@ -1,11 +1,12 @@
-from sys import argv
 from typing import List
-from typing import Optional
+from pathlib import Path
 
 from movs import read_txt
 from movs.model import Row
 
 from .tabui import main_window
+
+MOVS_DATA_ROOT = Path(__file__).parent.parent.parent / 'movs-data'
 
 
 def loader(path: str) -> List[Row]:
@@ -14,11 +15,6 @@ def loader(path: str) -> List[Row]:
 
 
 def main() -> None:
-    accumulator: Optional[str]
-    try:
-        _, accumulator = argv
-    except ValueError:
-        accumulator = None
-
-    with main_window(loader, accumulator) as window:
+    preload_path = str(MOVS_DATA_ROOT / 'BPOL_accumulator_vitomamma.txt')
+    with main_window(loader, preload_path) as window:
         window.show()
