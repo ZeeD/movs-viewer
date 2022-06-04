7f73a3b542fc87e8cc9a96c97bbd19107d90b397
Vito De Tullio <vito.detullio@gmail.com>
2022-06-04 10:51:33 +0200

    shut mypy

--- mypyui/mainui.py
+++ mypyui/mainui.py
@@ -1,10 +1,18 @@
+from typing import cast
+
 from PySide6.QtCore import QItemSelection
+from PySide6.QtGui import QAction
 from PySide6.QtGui import QKeySequence
 from PySide6.QtGui import QShortcut
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QDialogButtonBox
 from PySide6.QtWidgets import QFileDialog
+from PySide6.QtWidgets import QLineEdit
+from PySide6.QtWidgets import QMainWindow
 from PySide6.QtWidgets import QPlainTextEdit
+from PySide6.QtWidgets import QTableView
+from PySide6.QtWidgets import QToolButton
 from PySide6.QtWidgets import QWidget
 
 from .chartview import ChartView
@@ -14,6 +22,22 @@ from .settings import Settings
 from .viewmodel import SortFilterViewModel
 
 _dataPathsSeparator = '; \n'
+
+
+class Mainui(QMainWindow):
+    lineEdit: QLineEdit
+    tableView: QTableView
+    tab_2: QWidget
+    actionSettings: QAction
+    actionUpdate: QAction
+
+
+class Settingsui(QWidget):
+    usernameLineEdit: QLineEdit
+    passwordLineEdit: QLineEdit
+    dataPaths: QPlainTextEdit
+    buttonBox: QDialogButtonBox
+    openFileChooser: QToolButton
 
 
 def _setDataPaths(dataPaths: QPlainTextEdit, fileNames: list[str]) -> None:
@@ -34,13 +58,13 @@ def new_settingsui(settings: Settings) -> QWidget:
         fileNames, _ = QFileDialog.getOpenFileNames(settingsui)
         _setDataPaths(settingsui.dataPaths, fileNames)
 
-    settingsui = QUiLoader().load(SETTINGSUI_UI_PATH)
+    settingsui = cast(Settingsui, QUiLoader().load(SETTINGSUI_UI_PATH))
     settingsui.usernameLineEdit.setText(settings.username)
     settingsui.passwordLineEdit.setText(settings.password)
     _setDataPaths(settingsui.dataPaths, settings.data_paths)
 
-    settingsui.buttonBox.accepted.connect(save_settings)
-    settingsui.openFileChooser.clicked.connect(open_data_paths)
+    settingsui.buttonBox.accepted.connect(save_settings)  # type: ignore
+    settingsui.openFileChooser.clicked.connect(open_data_paths)  # type: ignore
 
     return settingsui
 
@@ -56,25 +80,25 @@ def new_mainui(settings: Settings,
                           _deselected: QItemSelection) -> None:
         model.selectionChanged(selection_model, mainui.statusBar())
 
-    mainui = QUiLoader().load(MAINUI_UI_PATH)
+    mainui = cast(Mainui, QUiLoader().load(MAINUI_UI_PATH))
 
     mainui.tableView.setModel(model)
     selection_model = mainui.tableView.selectionModel()
-    selection_model.selectionChanged.connect(update_status_bar)
+    selection_model.selectionChanged.connect(update_status_bar)  # type: ignore
 
-    mainui.lineEdit.textChanged.connect(model.filterChanged)
+    mainui.lineEdit.textChanged.connect(model.filterChanged)  # type: ignore
 
     chart_view = ChartView(settings)
     mainui.tab_2.layout().addWidget(chart_view)
 
-    mainui.actionUpdate.triggered.connect(update_helper)
-    mainui.actionSettings.triggered.connect(settingsui.show)
-    settingsui.accepted.connect(update_helper)
+    mainui.actionUpdate.triggered.connect(update_helper)  # type: ignore
+    mainui.actionSettings.triggered.connect(settingsui.show)  # type: ignore
+    settingsui.accepted.connect(update_helper)  # type: ignore
 
-    QShortcut(QKeySequence(mainui.tr('Ctrl+F')),
+    QShortcut(QKeySequence('Ctrl+F'),  # type: ignore
               mainui).activated.connect(mainui.lineEdit.setFocus)
-    QShortcut(QKeySequence(mainui.tr('Esc')),
-              mainui).activated.connect(lambda: mainui.lineEdit.setText)
+    QShortcut(QKeySequence('Esc'), mainui).activated.connect(  # type: ignore
+        lambda: mainui.lineEdit.setText(''))
 
     # on startup load
     update_helper()
