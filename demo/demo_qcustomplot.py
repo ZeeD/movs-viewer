e0d35233849c054f66598d83179c4a0b2e79ccc5
Vito De Tullio <vito.detullio@gmail.com>
2023-10-27 11:33:39 +0200

    dummy slider

--- demo/demo_qcustomplot.py
+++ demo/demo_qcustomplot.py
@@ -1,5 +1,5 @@
 from datetime import date
-from datetime import datetime
+from datetime import datetime, timedelta
 from datetime import time
 from decimal import Decimal
 from operator import attrgetter
@@ -9,9 +9,9 @@ from PyQt6.QtGui import QBrush
 from PyQt6.QtGui import QColor
 from PyQt6.QtGui import QPen
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtWidgets import QSlider
 from PyQt6.QtWidgets import QVBoxLayout
+from PyQt6.QtWidgets import QWidget
 from QCustomPlot_PyQt6 import QCP
 from QCustomPlot_PyQt6 import QCPAxisTickerDateTime
 from QCustomPlot_PyQt6 import QCustomPlot
@@ -19,15 +19,17 @@ from QCustomPlot_PyQt6 import QCustomPlot
 from movs import read_txt
 from movs.model import Rows
 
+from functools import partial
+
 
 def timestamp(d: date) -> float:
     return datetime.combine(d, time()).timestamp()
 
 
-def make_plot(rows: Rows) -> QCustomPlot:
-    customPlot = QCustomPlot()
+def make_plot(rows: Rows, parent: QWidget | None = None) -> QCustomPlot:
+    plot = QCustomPlot(parent)
 
-    graph = customPlot.addGraph()
+    graph = plot.addGraph()
     graph.setPen(QPen(Qt.GlobalColor.darkGreen))
     graph.setBrush(QBrush(QColor(0, 0, 255, 20)))
     graph.setName(rows.name)
@@ -37,25 +39,31 @@ def make_plot(rows: Rows) -> QCustomPlot:
         value = row.money if value is None else (value + row.money)
         graph.addData(timestamp(row.date), value)
 
-    customPlot.rescaleAxes()
-    customPlot.setInteraction(QCP.Interaction.iRangeDrag)
-    customPlot.setInteraction(QCP.Interaction.iRangeZoom)
-    customPlot.setInteraction(QCP.Interaction.iSelectPlottables)
+    plot.rescaleAxes()
+    plot.setInteraction(QCP.Interaction.iRangeDrag)
+    plot.setInteraction(QCP.Interaction.iRangeZoom)
+    plot.setInteraction(QCP.Interaction.iSelectPlottables)
 
     dateTicker = QCPAxisTickerDateTime()
     dateTicker.setDateTimeFormat('yyyy')
     dateTicker.setTickCount(rows[0].date.year-rows[-1].date.year)
-    customPlot.xAxis.setTicker(dateTicker)
-    # customPlot.xAxis.setRange(lower, upper)
+    plot.xAxis.setTicker(dateTicker)
+    # plot.xAxis.setRange(lower, upper)
     # setRangeLower
     # setRangeUpper
     # rescale
-    customPlot.xAxis.setTicks(True)
-    customPlot.xAxis.setSubTicks(True)
+    plot.xAxis.setTicks(True)
+    plot.xAxis.setSubTicks(True)
 
-    customPlot.legend.setVisible(True)
-    customPlot.legend.setBrush(QColor(255, 255, 255, 150))
-    return customPlot
+    plot.legend.setVisible(True)
+    plot.legend.setBrush(QColor(255, 255, 255, 150))
+    return plot
+
+
+def plotSetRangeLower(self: QCustomPlot, value: int)-> None:
+    d = date.fromordinal(value)
+    t = timestamp(d)
+    self.xAxis.setRangeLower(t)
 
 
 def main() -> None:
@@ -64,12 +72,18 @@ def main() -> None:
 
     app = QApplication([__file__])
 
-    mainapp = QMainWindow()
+    mainapp = QWidget()
 
-    plot = make_plot(rows)
-    slider = QSlider()
+    plot = make_plot(rows, mainapp)
+    slider = QSlider(Qt.Orientation.Horizontal, mainapp)
 
-    layout = QVBoxLayout()
+    slider.setMinimum(rows[-1].date.toordinal())
+    slider.setMaximum(rows[0].date.toordinal())
+    slider.setSingleStep(1)
+    slider.setPageStep(365)
+    slider.valueChanged.connect(partial(plotSetRangeLower, plot))
+
+    layout = QVBoxLayout(mainapp)
     layout.addWidget(plot)
     layout.addWidget(slider)
     mainapp.setLayout(layout)
