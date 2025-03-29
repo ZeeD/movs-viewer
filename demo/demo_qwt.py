f52b327d8f4fbd774841588eaa28a5cdc6c139a9
Vito De Tullio <vito.detullio@gmail.com>
2025-03-29 15:55:52 +0100

    working example

--- demo/demo_qwt.py
+++ demo/demo_qwt.py
@@ -10,6 +10,7 @@ from guilib.chartwidget.model import Column
 from guilib.chartwidget.model import ColumnHeader
 from guilib.chartwidget.model import ColumnProto
 from guilib.chartwidget.model import Info
+from guilib.chartwidget.model import InfoProto
 from guilib.chartwidget.viewmodel import SortFilterViewModel
 from guilib.qwtplot.plot import Plot
 from movslib.movs import read_txt
@@ -25,40 +26,39 @@ if TYPE_CHECKING:
     from movslib.model import Rows
 
 
-def acc(rows: 'Rows') -> 'Iterable[tuple[date, Decimal]]':
-    def func(a: 'tuple[date, Decimal]', b: 'Row') -> 'tuple[date, Decimal]':
-        return (b.date, a[1] + b.money)
+def load_infos(*fn_names: tuple[str, str]) -> list[InfoProto]:
+    def acc(rows: 'Rows') -> 'Iterable[tuple[date, Decimal]]':
+        def func(a: 'tuple[date, Decimal]', b: 'Row') -> 'tuple[date, Decimal]':
+            return (b.date, a[1] + b.money)
 
-    it = iter(sorted(rows, key=attrgetter('date')))
-    head = next(it)
-    return accumulate(it, func, initial=(head.date, head.money))
+        it = iter(sorted(rows, key=attrgetter('date')))
+        head = next(it)
+        return accumulate(it, func, initial=(head.date, head.money))
 
-
-def rowss2sort_filter_view_model(*rowss: 'Rows') -> SortFilterViewModel:
     tmp = defaultdict[date, list[ColumnProto]](list)
-    for rows in rowss:
+    for fn, name in fn_names:
+        _, rows = read_txt(fn, name)
         ch = ColumnHeader(rows.name)
         for d, m in acc(rows):
             tmp[d].append(Column(ch, m))
-    model = SortFilterViewModel()
-    model.update([Info(d, tmp[d]) for d in sorted(tmp)])
-    return model
+    return [Info(d, tmp[d]) for d in sorted(tmp)]
 
 
 def main() -> None:
-    _, rows_vm = read_txt(
-        '/home/zed/eclipse-workspace/movs-data/BPOL_accumulator_vitomamma.txt',
-        'vitomamma',
-    )
-    _, rows_ve = read_txt(
-        '/home/zed/eclipse-workspace/movs-data/BPOL_accumulator_vitoelena.txt',
-        'vitoelena',
-    )
-
-    model = rowss2sort_filter_view_model(rows_vm, rows_ve)
+    fn_names = [
+        (
+            '/home/zed/eclipse-workspace/movs-data/BPOL_accumulator_vitomamma.txt',
+            'vitomamma',
+        ),
+        (
+            '/home/zed/eclipse-workspace/movs-data/BPOL_accumulator_vitoelena.txt',
+            'vitoelena',
+        ),
+    ]
 
     app = QApplication(argv)
 
+    model = SortFilterViewModel()
     widget = QWidget()
     plot = Plot(model, None)
     chart_slider = ChartSlider(model, widget, dates_column=0)
@@ -72,7 +72,7 @@ def main() -> None:
 
     widget.show()
 
-    plot.model_reset()
+    model.update(load_infos(*fn_names))
 
     app.exec()
 
